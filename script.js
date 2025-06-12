4117990e28d6d635771813b2f27d735e24f8b4d3
laurasw14 <laura.swaricz@escola.pr.gov.br>
2025-06-12 15:28:13 -0300

    Aula 02
    Fiz o que foi pedido e mudei as cores

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
+
+function mostraPalavrasChave() { 
+  alert("fui clicado!"); 
+} 
+
+
+
