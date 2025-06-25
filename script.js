a52081f698f1efd9827c6e0e2cfee40e42d1dc71
laurasw14 <laura.swaricz@escola.pr.gov.br>
2025-06-25 17:58:35 +0000

    aula 6

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ const palavrasChave = processaTexto(texto);
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
+  const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
   let frequencias = {};
 
   for (let i of palavras) {
@@ -29,7 +41,5 @@ function processaTexto(texto) {
     }
   }
 
-  console.log(frequencias);
-
-  return palavras;
+  return frequencias;
 }
