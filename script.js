36c895456516e8835b0083666c6bf133b4cfea90
laurasw14 <laura.swaricz@escola.pr.gov.br>
2025-07-01 21:19:59 +0000

    atualizado script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
+
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
@@ -30,34 +31,20 @@ function processaTexto(texto) {
     palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+   
 
     function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
 
-    console.log(ordenadas);
+     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+   // console.log(ordenadas);
     return ordenadas.slice(0, 10);
 }
 
 
-
-
-function contaFrequencias(palavras) {
-  let frequencias = {};
-
-  for (let i of palavras) {
-    frequencias[i] = 0;
-
-    for (let j of palavras) {
-      if (i == j) {
-        frequencias[i]++;
-      }
-    }
-  }
-
-  return frequencias;
-}
 
 
 
@@ -69,4 +56,21 @@ function tiraPalavrasRuins(palavras) {
         }
     }
     return palavrasBoas;
+}
+
+
+
+function contaFrequencias(palavras) {
+    const frequencias = {};
+
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+
+    return frequencias;
 }
