6a3640a1860f82cba62f7454334b36596fca7bb5
laurasw14 <laura.swaricz@escola.pr.gov.br>
2025-07-01 21:59:26 +0000

    script

--- script.js
+++ script.js
@@ -19,13 +19,12 @@ function processaTexto(texto) {
   //let palavras = texto.split(/[^a-zA-Z]+/);
   let palavras = texto.split(/\P{L}+/u);
 
- let frequencias = [];
-
-  for (let i in palavras) {
+let frequencias = {};
+ for (let i of palavras) {
     frequencias[i] = 0;
 
-    for (let j in palavras) {
-      if (palavras[i] == palavras[j]) {
+    for (let j of palavras) {
+      if (i == j) {
         frequencias[i]++;
       }
     }
